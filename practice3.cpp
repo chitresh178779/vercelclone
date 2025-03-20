6b5ef14a6b12283019538b628c8a460ac26a40e2
Chitresh Gurjar <chitresh.gurjar8779@gmail.com>
2025-03-20 20:09:42 +0530

    deleted

--- practice3.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<"chitresh gujar";
-    cout<<"dakshat kalmodi";
-    cout<<"chintu parmar";
-    cout<<"kanika patil";
-    return 0;
-}
